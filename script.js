0544bce47e2007ca8278d0d1d2e78389ce48e403
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-10-02 13:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-//your JS code here. If required.
-let circle= document.querySelectorAll(".circle");
-let current =0;
-function updateCircle() {
-	circle.forEach((c,i)=>{
-		c.classList.toggle("active",i===current);
-	});
-	
+const circles = document.querySelectorAll(".circle");
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+let currentActive = 1;
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  prev.disabled = currentActive === 1;
+  next.disabled = currentActive === circles.length;
 }
-document.getElementById("next").addEventListener('click',()=>{
-	if(current<circle.length-1){
-		current++;
-		updateCircle();
-	}
-})
-document.getElementById("prev").addEventListener('click',()=>{
-	if(current>0)
-	{
-		current--;
-		updateCircle();
-	}
-})
+
+next.addEventListener("click", () => {
+  if (currentActive < circles.length) {
+    currentActive++;
+    update();
+  }
+});
+
+prev.addEventListener("click", () => {
+  if (currentActive > 1) {
+    currentActive--;
+    update();
+  }
+});
+
+update(); // initialize state
