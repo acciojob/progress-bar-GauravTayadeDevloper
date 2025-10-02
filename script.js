e3c2de349f0f9ca18872922caafd9907b4125e6b
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-10-02 13:02:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let circle= document.querySelectorAll(".circle");
+let current =0;
+function updateCircle() {
+	circle.forEach((c,i)=>{
+		c.classList.toggle("active",i===current);
+	})	
+}
+document.getElementById("next").addEventListener('click',()=>{
+	if(current<circle.length-1){
+		current++;
+		updateCircle();
+	}
+})
+document.getElementById("prev").addEventListener('click',()=>{
+	if(current>0)
+	{
+		current--;
+		updateCircle();
+	}
+})
